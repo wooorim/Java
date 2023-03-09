be6b575a6a7ac533b16659d91a05cf41d44a676d
woorim <122437236+wooorim@users.noreply.github.com>
2023-03-09 11:42:25 +0900

    Update Test.java
    update

--- src/github_test/Test.java
+++ src/github_test/Test.java
@@ -5,8 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello");
-		System.out.println("Hello");
-		System.out.println("Hello");
+		//System.out.println("Hello");
+		//System.out.println("Hello");
 		
 	}
 
