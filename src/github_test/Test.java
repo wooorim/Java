3ad3f28897df0c790e6b3ff8fac2665d27021cb5
woorim <122437236+wooorim@users.noreply.github.com>
2023-03-09 12:16:32 +0900

    Update Test.java
    commit

--- src/github_test/Test.java
+++ src/github_test/Test.java
@@ -3,10 +3,10 @@ package github_test;
 public class Test {
 
 	public static void main(String[] args) {
-		//System.out.println("Hello");
-		//System.out.println("Hello");
-		//System.out.println("Hello");
-		//System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
 		
 	}
 
